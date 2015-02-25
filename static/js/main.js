3ae781df56fb1f9341758f542c9a902d6a32ca95
Anoopsmohan <anoopmhn2009@gmail.com>
2015-02-25 16:43:40 +0530

    more changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,4 @@
 var fs = fs || {};
-var htmlBody = '';
 (function(fs) {
     fs.main = {
         init : function(){
@@ -7,29 +6,48 @@ var htmlBody = '';
         },
         loadImages: function(){
             var oThis = this;
-            $.when(
                 $.ajax({
                     url: '/get-images/',
                     error: function(){
                         console.log('Something wrong');
+                    },
+                    success: function(images){
+                         oThis.displayImage(images);
                     }
-                })
-            ).done(function(images){
-                    oThis.displayImage(images);
-                }
-            );
+                });
         },
 
         displayImage: function(images){
             //console.log(images.items);
             var imageList = images.items;
+            var htmlBody = "";
             console.log(imageList)
             for (lists in imageList){
                 console.log(imageList[lists].title);
                 //htmlBody += imageList[lists].title;
-                htmlBody += '<div><strong>'+imageList[lists].title+ '</strong><br><img src='+imageList[lists].url+'></div><br>';
+                htmlBody += [
+                        '<div class="test">',
+                            '<div class="fl" style="width:300px;">',
+                                '<img src='+imageList[lists].url+'>',
+                            '</div>',
+                            '<div class="fl" style="width:400px;">',
+                                '<span class="removeX" title="remove">X</span><br>',
+                                '<strong>',
+                                    imageList[lists].title,
+                                '</strong>',
+                            '</div>',
+                            '<div class="clear"></div>',
+                        '</div>'
+                ].join("")
             }
             $('#imageList').html(htmlBody);
+            $(".removeX").click(function(){
+                var el = $(this).closest(".test")
+                 el.fadeOut(1000, function(){
+                     el.remove();
+                 })
+
+            })
         }
     }
 
