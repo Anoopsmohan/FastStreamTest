e8a29c8a998fcc9cb6c17902bea4d6349e5c72e5
Anoopsmohan <anoopmhn2009@gmail.com>
2015-02-25 18:24:27 +0530

    Added view level caching

--- test_app/views.py
+++ test_app/views.py
@@ -1,9 +1,11 @@
-import simplejson
 import requests
+
+from django.views.decorators.cache import cache_page
 
 from decorators import jsonify
 
 
+@cache_page(60*15)
 @jsonify
 def get_images(request):
     res = requests.get('http://192.254.141.167/~fstech/pic.php')
